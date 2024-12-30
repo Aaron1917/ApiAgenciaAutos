dde00d3da5cf3fece9450ddcd3ed2ea858e94fe5
Aaron1917 <aaron1917@users.noreply.github.com>
2024-12-29 19:40:03 -0600

    metodos CRUD para marcas

--- src/main/java/com/mx/ApiRestAgenciaAutos/controller/MarcasWebService.java
+++ src/main/java/com/mx/ApiRestAgenciaAutos/controller/MarcasWebService.java
@@ -3,8 +3,12 @@ package com.mx.ApiRestAgenciaAutos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +21,54 @@ import com.mx.ApiRestAgenciaAutos.service.MarcasServiceImp;
 public class MarcasWebService {
 	@Autowired
 	MarcasServiceImp marcasImp;
-	
-	
+
 	// http://localhost:9000/MarcasWebService/listar
 	@GetMapping(path = "listar")
-	public List<Marcas> listar(){
+	public List<Marcas> listar() {
 		return marcasImp.listar();
+	}
+
+	// http://localhost:9000/MarcasWebService/guardar
+	@PostMapping(path = "guardar")
+	public ResponseEntity<?> guardar(@RequestBody Marcas marca) {
+		byte res = marcasImp.guardar(marca);
+		switch (res) {
+		case 0:
+			return new ResponseEntity<>(marca, HttpStatus.CREATED);
+		case 1:
+			return new ResponseEntity<>("Id repetido", HttpStatus.OK);
+		case 2:
+			return new ResponseEntity<>("Nobre de marca repetida", HttpStatus.OK);
+		case 3:
+			return new ResponseEntity<>("Id y Nombre repetidos", HttpStatus.OK);
+		default:
+			return new ResponseEntity<>("Error desconocido", HttpStatus.NO_CONTENT);
+		}
+	}
+
+	// http://localhost:9000/MarcasWebService/buscar
+	@PostMapping(path = "buscar")
+	public Marcas buscar(@RequestBody Marcas marca) {
+		return marcasImp.buscar(marca.getId());
+	}
+
+	// http://localhost:9000/MarcasWebService/editar
+	@PostMapping(path = "editar")
+	public ResponseEntity<?> editar(@RequestBody Marcas marca) {
+		boolean validar = marcasImp.editar(marca);
+		if (validar)
+			return new ResponseEntity<>(marca, HttpStatus.CREATED);
+		else
+			return new ResponseEntity<>("Id no encontrado", HttpStatus.NOT_FOUND);
+	}
+
+	// http://localhost:9000/MarcasWebService/eliminar
+	@PostMapping(path = "eliminar")
+	public ResponseEntity<?> eliminar(@RequestBody Marcas marca) {
+		boolean validar = marcasImp.eliminar(marca.getId());
+		if (validar)
+			return new ResponseEntity<>("Marca eliminada", HttpStatus.OK);
+		else
+			return new ResponseEntity<>("Id no encontrado", HttpStatus.NOT_FOUND);
 	}
 }
--- src/main/java/com/mx/ApiRestAgenciaAutos/service/MarcasServiceImp.java
+++ src/main/java/com/mx/ApiRestAgenciaAutos/service/MarcasServiceImp.java
@@ -11,12 +11,76 @@ import com.mx.ApiRestAgenciaAutos.repository.MarcasRepository;
 
 @Service
 public class MarcasServiceImp {
-	
+
 	@Autowired
 	MarcasRepository marcasRepo;
-	
+
+	// listar
 	@Transactional(readOnly = true)
-	public List<Marcas> listar(){
+	public List<Marcas> listar() {
 		return marcasRepo.findAll();
+
+	}
+	
+	// GUARDAR--VALIDAR QUE EL ID Y NOMBRE NO SE REPITAN
+	// 1: Id existente
+	// 2: Nombre existente
+	// 3: Nombre y Id existente
+	@Transactional
+	public Byte guardar(Marcas marca) {
+		byte retorno = 0;
+		boolean valorExistente = false;
+		for (Marcas m : marcasRepo.findAll()) {
+			if (m.getId().equals(marca.getId())) {
+				valorExistente = true;
+				retorno+=1;
+			}
+			if (m.getNombre().equalsIgnoreCase(marca.getNombre())) {
+				valorExistente = true;
+				retorno+=2;
+			}
+			if (valorExistente)
+				break;
+		}
+		if (!valorExistente)
+			marcasRepo.save(marca);
+		return retorno;
+	}
+
+	// BUSCAR
+	@Transactional(readOnly = true)
+	public Marcas buscar(long id) {
+		return marcasRepo.findById(id).orElse(null);
+	}
+	
+	
+	// EDITAR---VALIDAR QUE EL ID EXISTA PARA PODER EDITAR
+	@Transactional
+	public boolean editar(Marcas marca) {
+		boolean marcaExistente = false;
+		for (Marcas m : marcasRepo.findAll()) {
+			if (m.getId().equals(marca.getId())) {
+				marcaExistente = true;
+				break;
+			}
+		}
+		if (marcaExistente)
+			marcasRepo.save(marca);
+		return marcaExistente;
+	}
+	
+	// ELIMINAR----VALIDAR QUE EL ID EXISTA PARA PODER ELIMINAR
+	@Transactional
+	public boolean eliminar(long id) {
+		boolean marcaExistente = false;
+		for (Marcas m : marcasRepo.findAll()) {
+			if (m.getId().equals(id)) {
+				marcaExistente = true;
+				break;
+			}
+		}
+		if (marcaExistente)
+			marcasRepo.deleteById(id);;
+		return marcaExistente;
 	}
 }
